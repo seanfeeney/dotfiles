0948f48b0b353aa70cddd12c78ff759d9aed1871
Sean Feeney <feeney22@gmail.com>
2019-04-07 12:00:29 -0400

    fixing install

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+
+
 # Get dotfiles installation directory
-DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+git clone https://github.com/romkatv/powerlevel10k.git ~/oh-my-zsh/themes/powerlevel10k
 
-ln -sf "$DOTFILES_DIR/.gitconfig" ~
-ln -sf "$DOTFILES_DIR/.gitignore_global" ~
-ln -sf "$DOTFILES_DIR/.zshrc" ~
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+# setup vim plugin manager
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
