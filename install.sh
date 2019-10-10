a2ba9aa862810f2311f19b5e1acdac375d862000
sean <feeney22@gmail.com>
2019-10-09 21:51:40 -0400

    dot files changes

--- install.sh
+++ install.sh
@@ -18,7 +18,6 @@ cd ~
 
 # Get dotfiles installation directory
 git clone https://github.com/robbyrussell/oh-my-zsh.git || true
-sudo git clone https://github.com/romkatv/powerlevel10k.git ~/oh-my-zsh/themes/powerlevel10k || true
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/oh-my-zsh/custom}/plugins/zsh-syntax-highlighting || true
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/oh-my-zsh/custom}/plugins/zsh-autosuggestions || true
 
@@ -30,5 +29,5 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 
 
 # setup aliases for dotfile locations
-grep -qxF 'source ~/dotfiles/vimrc/vimrc' ~/.vimrc || echo 'source ~/dotfiles/vimrc/vimrc' >> ~/.vimrc
+grep -qxF 'source ~/dotfiles/vimrc' ~/.vimrc || echo 'source ~/dotfiles/vimrc/vimrc' >> ~/.vimrc
 grep -qxF 'source ~/dotfiles/zshrc' ~/.zshrc || echo 'source ~/dotfiles/zshrc' >> ~/.zshrc
